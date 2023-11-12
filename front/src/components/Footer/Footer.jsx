5178b2b498331f96eea65be0e33e502c35ec5c09
lucas.cepeda2 <lucas.cepeda2@gmail.com>
2023-11-12 18:37:38 -0300

    new footer

--- front/src/components/Footer/Footer.jsx
+++ front/src/components/Footer/Footer.jsx
@@ -5,37 +5,36 @@ import './footer.css'
 
 
 function Footer() {
-  return (
-    <footer className="footer-container">
-
-      <div className="footer-logo">
-        <img src='./src/assets/logo.png' alt='logo' />
-        <p> © 2023 Equipo2. Todos los derechos reservados. </p>
-      </div>
-      <div className='footer-social-media'>
-      <a href="https://twitter.com/home?lang=es" target="_blank" rel="noopener noreferrer">
-    <img src= '.\src\components\Footer\socialmediaimg\twitter.svg' alt= 'X'/>
-  </a>
-  <br />
-  <br />
-  <a href="https://www.youtube.com/" target="_blank" rel="noopener noreferrer">
-    <img src= './src/components/Footer/socialmediaimg/youtube.svg' alt= 'X'/>
-  </a>
-  <br />
-  <br />
-  <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
-    <img src= '.\src\components\Footer\socialmediaimg\instagram.svg' alt= 'X'/>
-  </a>
-  <br />
-  <br />
-  <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
-    <img src= '.\src\components\Footer\socialmediaimg\facebook.svg' alt= 'X'/>
-  </a>
-</div>
-
-      <div className="footer-info"><strong>Contacto</strong><br /><br /><p>equipo2@gmail.com<br /><br />11 9875-4987<br /><br />Echeverria 345, UY</p></div>
-    </footer>
-  );
+    return (
+        <footer className="footer-container">
+            <div className="footer-logo">
+                <img src='./src/assets/solo-logo.jpeg' alt='logo' />
+                <p> © 2023 Equipo2. Todos los derechos reservados. </p>
+            </div>
+            <div className='side-right'>
+                <div className='footer-social-media'>
+                    <a href="https://twitter.com/home?lang=es" target="_blank" rel="noopener noreferrer">
+                        <img src='.\src\components\Footer\socialmediaimg\twitter.svg' alt='X' />
+                    </a>
+                    <a href="https://www.youtube.com/" target="_blank" rel="noopener noreferrer">
+                        <img src='./src/components/Footer/socialmediaimg/youtube.svg' alt='X' />
+                    </a>
+                    <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+                        <img src='.\src\components\Footer\socialmediaimg\instagram.svg' alt='X' />
+                    </a>
+                    <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
+                        <img src='.\src\components\Footer\socialmediaimg\facebook.svg' alt='X' />
+                    </a>
+                </div>
+                <div className="footer-info">
+                    <strong>Contacto</strong>
+                    <p>equipo2@gmail.com</p>
+                    <p>11 9875-4987</p>
+                    <p>Echeverria 345, UY</p>
+                </div>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer
